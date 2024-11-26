5a0e0f6e71ead2fad81e0b12c6b08fe8de836c87
tm2463 <tm2463@bath.ac.uk>
2024-11-26 12:51:15 +0000

    Delete reverse_complement.py

--- reverse_complement.py
+++
@@ -1,8 +0,0 @@
-from Bio.Seq import Seq
-import os
-
-with open((os.path.expanduser('~/Desktop/rosalind_revc.txt')), 'r') as f:
-    file = f.read().replace('\n', '')
-
-my_seq = Seq(file)
-print(my_seq.reverse_complement())
