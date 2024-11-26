028748c5388930598ca3b603579e0184f9d9ffd6
tm2463 <tm2463@bath.ac.uk>
2024-11-26 12:51:05 +0000

    Delete translating_protein.py

--- translating_protein.py
+++
@@ -1,8 +0,0 @@
-import os
-from Bio.Seq import Seq
-
-with open((os.path.expanduser('~/Desktop/rosalind_prot.txt')), 'r') as f:
-    file = f.read().replace('\n', '')
-
-RNA = Seq(file)
-print(RNA.translate()[:-1]) # remove asterix marking end codon
